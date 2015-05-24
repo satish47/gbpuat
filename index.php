9ffc4aa783854bb36c5bb7b2fee04e1015b2fd84
Satish Rana <rsatish931@gmail.com>
2015-05-24 19:30:58 +0530

    upload

---
+++ index.php
@@ -0,0 +1,245 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0 user-scalable=no"/>
+
+	<title>FreeThemes</title>
+
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="css/style.css"  media="screen,projection"/>
+
+	<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+</head>
+
+<body class="light blue lighten-4">
+	<div class="navbar-fixed">
+    <ul id="dropdown1" class="dropdown-content teal lighten-2">
+      <li><a href="#!">Bootstrap</a></li>
+      <li><a href="#!">Materializecss</a></li>
+    </ul>
+    <nav class="teal lighten-1" role="navigation">
+      <div class="nav-wrapper container">
+        <a href="#" class="brand-logo">FreeThemes</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#" class="dropdown-button" data-activates="dropdown1">Themes<i class="mdi-navigation-arrow-drop-down right"></i></a></li>
+          <li><a href="#">Tutorial</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <ul id="dropdown2" class="dropdown-content">
+      <li><a href="#!">Bootstrap</a></li>
+      <li><a href="#!">Materializecss</a></li>
+    </ul>
+    <ul class="side-nav" id="mobile-demo">
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#" class="dropdown-button" data-activates="dropdown2">Themes<i class="mdi-navigation-arrow-drop-down right"></i></a></li>
+      <li><a href="#">Tutorial</a></li>
+    </ul>
+
+  </div>
+
+  <div class="parallax-container" id="index-banner">
+    <div class="section no-pad-bot">
+    <div class="container">
+        <h1 class="header center teal-text text-lighten-2">THEMES</h1>
+      <div class="row center">
+        <a href="#" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">BOOTSTRAP</a>
+        <a href="#" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">MATERIALIZECSS</a>
+      </div>
+    </div>
+    </div>
+    <div class="parallax responsive-img"><img src="images/header1.jpg"></div>
+  </div>
+
+<footer class="teal">
+  <div class="footer-copyright">
+    <div class="container">
+      <a class="white-text text-lighten-3" href="#">All Themes</a>
+    </div>
+  </div>
+</footer>
+
+<div class="container">
+  <div class="row">
+    <div class="col l4 m4 s12">
+      <div class="card">
+        <div class="card-image waves-effect waves-light">
+          <img class="activator" src="images/shruti.jpg">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title <i class="mdi-navigation-close right"></i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          <a class="waves-effect waves-light btn">Preview</a>
+          <a class="waves-effect waves-light btn">Download</a>
+        </div>
+      </div>
+    </div>
+    <div class="col l4 m4 s12 ">
+      <div class="card">
+        <div class="card-image waves-effect waves-light">
+          <img class="activator" src="images/shruti.jpg">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title <i class="mdi-navigation-close right"></i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          <a class="waves-effect waves-light btn">Preview</a>
+          <a class="waves-effect waves-light btn">Download</a>
+        </div>
+      </div>
+    </div>
+    <div class="col l4 m4 s12">
+      <div class="card">
+        <div class="card-image waves-effect waves-light">
+          <img class="activator" src="images/shruti.jpg">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title <i class="mdi-navigation-close right"></i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          <a class="waves-effect waves-light btn">Preview</a>
+          <a class="waves-effect waves-light btn">Download</a>
+        </div>
+      </div>
+    </div>
+    <div class="col l4 m4 s12">
+      <div class="card">
+        <div class="card-image waves-effect waves-light">
+          <img class="activator" src="images/shruti.jpg">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title <i class="mdi-navigation-close right"></i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          <a class="waves-effect waves-light btn">Preview</a>
+          <a class="waves-effect waves-light btn">Download</a>
+        </div>
+      </div>
+    </div>
+    <div class="col l4 m4 s12">
+      <div class="card">
+        <div class="card-image waves-effect waves-light">
+          <img class="activator" src="images/shruti.jpg">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title <i class="mdi-navigation-close right"></i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          <a class="waves-effect waves-light btn">Preview</a>
+          <a class="waves-effect waves-light btn">Download</a>
+        </div>
+      </div>
+    </div>
+    <div class="col l4 m4 s12">
+      <div class="card">
+        <div class="card-image waves-effect waves-light">
+          <img class="activator" src="images/shruti.jpg">
+        </div>
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">Card Title <i class="mdi-navigation-more-vert right"></i></span>
+        </div>
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">Card Title <i class="mdi-navigation-close right"></i></span>
+          <p>Here is some more information about this product that is only revealed once clicked on.</p>
+          <a class="waves-effect waves-light btn">Preview</a>
+          <a class="waves-effect waves-light btn">Download</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="teal">
+  <div class="footer-copyright">
+    <div class="container">
+      <a class="white-text text-lighten-3" href="#">My Works</a>
+    </div>
+  </div>
+</footer>
+
+<footer class="page-footer teal">
+  <div class="container">
+    <div class="row">
+      <div class=" row col m6 s12">
+        <h5 class="white-text">About</h5>
+        <p class="grey-text text-lighten-4">Here you find free responsive web templates using bootstrap and materializecss</p>
+        <div class="valign-wrapper">
+          <div class="col s4">
+            <img src="images/me.jpg" class="circle responsive-img">
+          </div>
+          <div class="col s8" id="links">
+            <ul class="">
+              <li><a href="#"><img src="images/facebook.png" alt="Facebook" width="30" height="30"/></a></li>
+              <li><a href="#"><img src="images/twitter.png" alt="Twitter" width="30" height="30"/></a></li>
+              <li><a href="#"><img src="images/google-plus.png" alt="Google+" width="30" height="30"/></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="row col m6 s12">
+        <h5 class="white-text">Contact</h5>
+        <form>
+          <div class="row">
+              <div class="input-field col s12 m6">
+                <i class="mdi-social-person prefix white-text"></i>
+                <input id="fullname" type="text" class="validate white-text">
+                <label for="fullname" class="white-text">Name</label>
+              </div>
+              <div class="input-field col s12 m6">
+                <i class="mdi-content-mail prefix white-text"></i>
+                <input id="email" type="email" class="validate white-text">
+                <label for="email" class="white-text">Email ID</label>
+              </div>
+              <div class="input-field col s12 m12">
+                <i class="mdi-editor-mode-edit prefix white-text"></i>
+                <textarea id="icon-prefix2" class="materialize-textarea white-text"></textarea>
+                <label for="icon-prefix2" class="white-text">Message</label>
+              </div>
+              <div class="input-field col s12 m12">
+                <button class="btn waves-effect waves-light green right" type="submit">SEND
+                  <i class="mdi-content-send right"></i>
+                </button>
+              </div>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="" style="margin-left:20px; margin-right:20px;"><span>@copyright<a class="brown-text text-lighten-3" href="#"> FreeThemes (Satish Rana)</a></span>
+    <div class="white-text text-lighten-3 right"><i class="mdi-communication-call"></i>9012667781</div>
+    </div>
+  </div>
+</footer>
+
+  <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <script>
+    $( document ).ready(function(){
+      $(".button-collapse").sideNav();
+      $(".dropdown-button").dropdown({hover: false});
+      $('.parallax').parallax();
+    })
+  </script>
+</body>
+</html>
